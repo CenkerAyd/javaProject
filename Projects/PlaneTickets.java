ed976ed872e432aa094b0017992057740f88a40c
Cenker Aydın <112515788+CenkerAyd@users.noreply.github.com>
2022-11-14 21:07:58 +0300

    Update PlaneTickets.java

--- Projects/PlaneTickets.java
+++ Projects/PlaneTickets.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class PlaneTickets {
+    
+    //Get Distance (KM), age and trip type (One Way, Round-Round) information from the user. Take the fare per distance as 0.10 TL / km. First, calculate the total price of the flight and then apply the following discounts to the customer according to the conditions;
+
+    //Values received from the user must be valid (distance and age are positive numbers, trip type is 1 or 2). Otherwise, "You Have Entered Wrong Data!" A warning should be given.
+    //If the person is younger than 12 years old, 50% discount is applied on the ticket price.
+    //If the person is between the ages of 12-24, a 10% discount is applied on the ticket price.
+    //If the person is over 65 years old, a 30% discount is applied on the ticket price.
+    //If the person has chosen the "Journey Type" round trip, 20% discount is applied on the ticket price.
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the the Km:");
